9d71b5de914de618d76c6ef762b04f5b43628e58
Steve Laing <steve.laing@gmail.com>
2026-01-09 17:30:30 +0000

    Add relay listener integration test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,8 @@
 import shutil
 import sys
+from contextlib import contextmanager
 from pathlib import Path
+from unittest.mock import AsyncMock, patch
 
 import pytest
 
@@ -18,3 +20,45 @@ def teardown(tmp_dir):
 
     yield
     shutil.rmtree(tmp_dir)
+
+
+class FakeWebSocket:
+    """Async iterator + websocket mock."""
+
+    def __init__(self, messages):
+        self._messages = messages
+        self.send = AsyncMock()
+        self.recv = AsyncMock()
+
+    def __aiter__(self):
+        self._iter = iter(self._messages)
+        return self
+
+    async def __anext__(self):
+        try:
+            return next(self._iter)
+        except StopIteration:
+            raise StopAsyncIteration
+
+
+@contextmanager
+def fake_relay_contextmanager(relay_message, client_payload):
+    relay_ws = FakeWebSocket([relay_message])
+    client_ws = FakeWebSocket([])
+    client_ws.recv.return_value = client_payload
+
+    relay_cm = AsyncMock()
+    relay_cm.__aenter__.return_value = relay_ws
+    relay_cm.__aexit__.return_value = None
+
+    client_cm = AsyncMock()
+    client_cm.__aenter__.return_value = client_ws
+    client_cm.__aexit__.return_value = None
+
+    with patch("relay_listener.connect", side_effect=[relay_cm, client_cm]):
+        yield client_ws
+
+
+@pytest.fixture
+def fake_relay():
+    return fake_relay_contextmanager
